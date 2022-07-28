776bc05ac92a11b99a6cedeaeeb87611ec57fea3
leastchaos <leastchaos@gmail.com>
2022-07-28 10:11:06 +0000

    update hb_app

--- src/app.py
+++ src/app.py
@@ -7,6 +7,12 @@ from .wallet.app import app as wallet_app
 app = FastAPI()
 app.include_router(exchange_app)
 app.include_router(wallet_app)
+try:
+    from src.hb_app.app import app as hb_app
+
+    app.include_router(hb_app)
+except ImportError:
+    pass
 
 
 @app.get("/")
