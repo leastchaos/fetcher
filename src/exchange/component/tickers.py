50d134a03272c1e0ec882a89c7e8991cf70fb967
leastchaos <leastchaos@gmail.com>
2022-07-28 17:00:51 +0000

    add tickers

---
+++ src/exchange/component/tickers.py
@@ -0,0 +1,25 @@
+"""fetch tickers"""
+import asyncio
+
+import ccxt.async_support as ccxt
+import redis
+
+from src.exchange.utils import push_data, safe_timeout_method
+
+
+async def fetch_tickers_loop(client: ccxt.Exchange, db: redis.Redis) -> None:
+    """fetch loans"""
+    name = client.options["name"]
+    while True:
+        data = await safe_timeout_method(client.fetch_tickers)
+        if data:
+            push_data(db, "tickers", name, data)
+        await asyncio.sleep(60)
+
+
+def start_tickers_loop(clients: ccxt.Exchange, db: redis.Redis) -> list[asyncio.Task]:
+    """start loans loop"""
+    return [
+        asyncio.create_task(fetch_tickers_loop(client, db))
+        for client in clients.values()
+    ]
